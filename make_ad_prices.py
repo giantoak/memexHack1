151f085c0a4f86541aebf90582bd01ae706b0a13
Pete[r] M. Landwehr <pmlandwehr@gmail.com>
2015-10-07 12:49:32 -0400

    added main function

--- make_ad_prices.py
+++ make_ad_prices.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-
+# TODO Update docstring to match reality
 """
 This script takes ad price extraction and creates ad_prices_ad_level.csv (ad level clean price data)
 
 It then creates a data set of only ads with two prices "doubles" with 
-the implied fixed cost 
+the implied fixed cost
 """
 from AdPriceHelper import basic_ad_id_loader
 from AdPriceHelper import all_call_merge
@@ -13,116 +13,121 @@ import pandas as pd
 import numpy as np
 nrows = None
 
-# Re-read ad_prices_price_level.csv to aggregate from file
-data = pd.read_csv('ad_prices_price_level.csv')
-print(data.shape)
-# Begin computing price per hour:
-# If we have a 1 hour price, that's it. Otherwise, multiply all the
-# quoted prices by a 'multiplier' which represents the average ratio of
-# hourly price to the given time period price
 
-# The below blocks of code compute 'price_ratios' which is the ratio of
-# average prices for 1 hour for other ads that also posted the same
-# price
-minute_values = pd.Series((data['minutes'].value_counts()/data.shape[0] > .0001).index.map(int))
-minute_string_series = minute_values.map(lambda x: 'price_%s_mins' % x)
-minute_string_series.index = minute_values
+def main():
 
+    # Re-read ad_prices_price_level.csv to aggregate from file
+    data = pd.read_csv('ad_prices_price_level.csv')
+    print(data.shape)
+    # Begin computing price per hour:
+    # If we have a 1 hour price, that's it. Otherwise, multiply all the
+    # quoted prices by a 'multiplier' which represents the average ratio of
+    # hourly price to the given time period price
 
-def get_prices(x):
-    temp = pd.Series(np.nan, index=minute_values)
-    for mins in minute_values:
-        matching_prices = x[x['minutes'] == mins]['price']
-        if len(matching_prices):
-            temp[mins] = matching_prices.mean()
-    return temp
+    # The below blocks of code compute 'price_ratios' which is the ratio of
+    # average prices for 1 hour for other ads that also posted the same
+    # price
+    minute_values = pd.Series((data['minutes'].value_counts()/data.shape[0] > .0001).index.map(int))
+    minute_string_series = minute_values.map(lambda x: 'price_%s_mins' % x)
+    minute_string_series.index = minute_values
 
-me = data.groupby('ad_id').apply(get_prices)  # This is REALLLLY slow
-price_ratios = pd.Series(np.nan, index=minute_values)
-price_ratios_counts = pd.Series(np.nan, index=minute_values)
-for m in minute_values:
-    hour_price = me[(~me[60].isnull()) & (~me[m].isnull())][60].mean()
-    m_price = me[(~me[m].isnull()) & (~me[m].isnull())][m].mean()
-    price_ratios[m] = hour_price/m_price
-    price_ratios_counts[m] = me[(~me[m].isnull()) & (~me[m].isnull())].shape[0]
+    def get_prices(x):
+        temp = pd.Series(np.nan, index=minute_values)
+        for mins in minute_values:
+            matching_prices = x[x['minutes'] == mins]['price']
+            if len(matching_prices):
+                temp[mins] = matching_prices.mean()
+        return temp
 
-print('Computed price ratios for acts of given length to acts of 1 hour')
-print(price_ratios)
-print(price_ratios_counts)
+    me = data.groupby('ad_id').apply(get_prices)  # This is REALLLLY slow
+    price_ratios = pd.Series(np.nan, index=minute_values)
+    price_ratios_counts = pd.Series(np.nan, index=minute_values)
+    for m in minute_values:
+        hour_price = me[(~me[60].isnull()) & (~me[m].isnull())][60].mean()
+        m_price = me[(~me[m].isnull()) & (~me[m].isnull())][m].mean()
+        price_ratios[m] = hour_price/m_price
+        price_ratios_counts[m] = me[(~me[m].isnull()) & (~me[m].isnull())].shape[0]
 
-# Now split the data by whether there's a posted price of 1 hr
-data['1hr'] = data['time_str'] == '1 HOUR'
-a = data.groupby('ad_id')['1hr'].sum()
-a = a > 0
-del data['1hr']
-a = pd.DataFrame(a)
-data = pd.merge(data, a, left_on='ad_id', right_index=True)
-price_level_hourly = pd.DataFrame(data[data['1hr']])
-price_level_hourly['price_per_hour'] = price_level_hourly['price']  # If there's an hourly price, use it
-price_level_no_hourly = pd.DataFrame(data[~data['1hr']])
-price_level_no_hourly.index = price_level_no_hourly['ad_id']
-# Otherwise use the multiplier
-price_level_no_hourly['multiplier'] = price_level_no_hourly['minutes'].apply(lambda x: price_ratios[x])
-price_level_no_hourly['price_per_hour'] = price_level_no_hourly['price'] * price_level_no_hourly['multiplier']
-price_level_no_hourly_prices = pd.DataFrame(price_level_no_hourly.groupby('ad_id')['price_per_hour'].mean())
-price_level_no_hourly['price_per_hour'] = price_level_no_hourly_prices
-price_level = pd.concat([price_level_hourly, price_level_no_hourly], axis=0)
-price_level.sort('1hr', ascending=False, inplace=True)
-ad_level_prices = pd.DataFrame(price_level.groupby('ad_id')['price_per_hour'].mean(), columns=['price_per_hour'])
-ad_level = price_level.drop_duplicates('ad_id')[['ad_id', 'sex_ad', 'census_msa_code', 'cluster_id', 'date_str',
-                                                 'is_massage_parlor_ad', '1hr', 'incall', 'no_incall', 'outcall',
-                                                 'no_outcall', 'incalloutcall', 'no_incalloutcall']]
-out = pd.merge(ad_level_prices, ad_level, left_index=True, right_on='ad_id', how='left')
-# Clean up some unused data...
-print('cleaning up old data...')
-del data
-del price_level
-del ad_level
-del ad_level_prices
+    print('Computed price ratios for acts of given length to acts of 1 hour')
+    print(price_ratios)
+    print(price_ratios_counts)
 
-# Filter out spam guys with > 200 ads in our sample period and save
-spam = pd.DataFrame(out.groupby('cluster_id').apply(lambda x: x.shape[0] > 200), columns=['spam'])
-spam.reset_index(inplace=True)
-out = out.merge(spam)
+    # Now split the data by whether there's a posted price of 1 hr
+    data['1hr'] = data['time_str'] == '1 HOUR'
+    a = data.groupby('ad_id')['1hr'].sum()
+    a = a > 0
+    del data['1hr']
+    a = pd.DataFrame(a)
+    data = pd.merge(data, a, left_on='ad_id', right_index=True)
+    price_level_hourly = pd.DataFrame(data[data['1hr']])
+    price_level_hourly['price_per_hour'] = price_level_hourly['price']  # If there's an hourly price, use it
+    price_level_no_hourly = pd.DataFrame(data[~data['1hr']])
+    price_level_no_hourly.index = price_level_no_hourly['ad_id']
+    # Otherwise use the multiplier
+    price_level_no_hourly['multiplier'] = price_level_no_hourly['minutes'].apply(lambda x: price_ratios[x])
+    price_level_no_hourly['price_per_hour'] = price_level_no_hourly['price'] * price_level_no_hourly['multiplier']
+    price_level_no_hourly_prices = pd.DataFrame(price_level_no_hourly.groupby('ad_id')['price_per_hour'].mean())
+    price_level_no_hourly['price_per_hour'] = price_level_no_hourly_prices
+    price_level = pd.concat([price_level_hourly, price_level_no_hourly], axis=0)
+    price_level.sort('1hr', ascending=False, inplace=True)
+    ad_level_prices = pd.DataFrame(price_level.groupby('ad_id')['price_per_hour'].mean(), columns=['price_per_hour'])
+    ad_level = price_level.drop_duplicates('ad_id')[['ad_id', 'sex_ad', 'census_msa_code', 'cluster_id', 'date_str',
+                                                     'is_massage_parlor_ad', '1hr', 'incall', 'no_incall', 'outcall',
+                                                     'no_outcall', 'incalloutcall', 'no_incalloutcall']]
+    out = pd.merge(ad_level_prices, ad_level, left_index=True, right_on='ad_id', how='left')
+    # Clean up some unused data...
+    print('cleaning up old data...')
+    del data
+    del price_level
+    del ad_level
+    del ad_level_prices
 
-# Add site filter
-out['site'] = out['ad_id'].apply(lambda x: x.split(':')[0])
+    # Filter out spam guys with > 200 ads in our sample period and save
+    spam = pd.DataFrame(out.groupby('cluster_id').apply(lambda x: x.shape[0] > 200), columns=['spam'])
+    spam.reset_index(inplace=True)
+    out = out.merge(spam)
 
-# Compute the cluster size
-out = out.merge(out.groupby('cluster_id').size().to_frame('cluster_count').reset_index())
-out.to_csv('ad_price_ad_level.csv', index=False)
+    # Add site filter
+    out['site'] = out['ad_id'].apply(lambda x: x.split(':')[0])
 
-# Now begin rebuilding DF by merging in original raw files, so we can
-# see how much stuff is missing...
-del out['cluster_id']
-del out['date_str']
-ts = basic_ad_id_loader(
-    ['data/forGiantOak3/doc-provider-timestamp.tsv', 'data/forGiantOak3/doc-provider-timestamp.tsv.gz'],
-    ['ad_id', 'cluster_id', 'date_str'],
-    nrows)
-out = ts.merge(out, how='outer')
-del ts
+    # Compute the cluster size
+    out = out.merge(out.groupby('cluster_id').size().to_frame('cluster_count').reset_index())
+    out.to_csv('ad_price_ad_level.csv', index=False)
 
-del out['census_msa_code']
-msa = basic_ad_id_loader(
-    ['data/forGiantOak3/msa_locations.tsv', 'data/forGiantOak3/msa_locations.tsv.gz'],
-    ['ad_id', 'census_msa_code'],
-    nrows)
-out = msa.merge(out, how='outer')
-del msa
+    # Now begin rebuilding DF by merging in original raw files, so we can
+    # see how much stuff is missing...
+    del out['cluster_id']
+    del out['date_str']
+    ts = basic_ad_id_loader(
+        ['data/forGiantOak3/doc-provider-timestamp.tsv', 'data/forGiantOak3/doc-provider-timestamp.tsv.gz'],
+        ['ad_id', 'cluster_id', 'date_str'],
+        nrows)
+    out = ts.merge(out, how='outer')
+    del ts
 
-# Merge in massage parlor flag
-del out['is_massage_parlor_ad']
-massage = basic_ad_id_loader(
-    ['data/forGiantOak3/ismassageparlorad.tsv'],
-    ['ad_id', 'is_massage_parlor_ad'],
-    nrows)
-out = out.merge(massage, how='right')
-del massage
+    del out['census_msa_code']
+    msa = basic_ad_id_loader(
+        ['data/forGiantOak3/msa_locations.tsv', 'data/forGiantOak3/msa_locations.tsv.gz'],
+        ['ad_id', 'census_msa_code'],
+        nrows)
+    out = msa.merge(out, how='outer')
+    del msa
 
-del out['incall']
-del out['outcall']
-del out['incalloutcall']
-out = all_call_merge(out, 'right', nrows)
+    # Merge in massage parlor flag
+    del out['is_massage_parlor_ad']
+    massage = basic_ad_id_loader(
+        ['data/forGiantOak3/ismassageparlorad.tsv'],
+        ['ad_id', 'is_massage_parlor_ad'],
+        nrows)
+    out = out.merge(massage, how='right')
+    del massage
 
-out.to_csv('ad_price_ad_level_all.csv', index=False)
+    del out['incall']
+    del out['outcall']
+    del out['incalloutcall']
+    out = all_call_merge(out, 'right', nrows)
+
+    out.to_csv('ad_price_ad_level_all.csv', index=False)
+
+if __name__ == "__main__":
+    main()
